a44eaa2c9998b667961b6397e9eb5a05c88dd388
layeeque <layeequerehman786@gmail.com>
2017-05-31 16:00:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(bodyParser.urlencoded({
 restService.use(bodyParser.json());
 
 restService.post('/echo', function(req, res) {
-
+        var obj="";
      var q = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     var api = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAbMVp_Kmi6Ixrh6RfVLAbW_mMAY0O7itQ&cx=013351406654656600973:5gj2eij-z1i&q=encodeURIComponent("+q+")";
     
@@ -26,7 +26,7 @@ restService.post('/echo', function(req, res) {
                   console.log("body: "+body);
              //  var bodya = body.searchInformation.totalResults;
                    //return res.json(bodya);
-                 var obj =JSON.parse(body).items;
+                 obj =JSON.parse(body).items;
                 // var obj1=JSON.parse(obj).title;
               //  console.log("===================================================================================");   
                 
